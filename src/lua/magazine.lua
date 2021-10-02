9b413b0e3adf3430074f57db4d5d20d095b7f8ed
Alayna T <alayna2198@gmail.com>
2021-10-02 12:16:31 -0500

    Add magazine subsystem
    oops i forgot to push this like 2 weeks ago but i dont think itll interfere also i never had a chance to test it

---
+++ src/lua/magazine.lua
@@ -0,0 +1,32 @@
+    local magazineMotor = WPI_TalonSRX:new(7)
+    local otherMagazineMotor = WPI_TalonSRX:new(6)
+
+    otherMagazineMotor:setInverted(CTREInvertType.InvertMotorOutput);
+    magazineMotor:setInverted(CTREInvertType.InvertMotorOutput);
+
+
+    -- ✩  ✩
+    function magazineRollIn() 
+        magazineMotor:set(0.87)
+        otherMagazineMotor:set(0.87)
+    end
+    
+    function stopMagazine() 
+        magazineMotor:set(0)
+        otherMagazineMotor:set(0)
+    end
+    
+    -- ✩ Actually just sets speed of magazine motor ✩
+    -- @param speed speed to set magazine motor to
+    
+    function setMagazineMotor(speed) 
+        magazineMotor:set(speed)
+        otherMagazineMotor:set(speed)
+    end
+
+    function magazineRollOut() 
+        magazineMotor:set(-1)
+        otherMagazineMotor:set(-1)
+    end
+
+    
