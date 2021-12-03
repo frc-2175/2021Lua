86d43cc7182b081a063370b0e4fcf229aab3db4e
driverstation <info@fightingcalculators.org>
2021-12-02 18:00:03 -0600

    Update drive code to arcade drive

--- src/lua/robot.lua
+++ src/lua/robot.lua
@@ -6,11 +6,11 @@ function robot.robotInit()
     rightStick = Joystick:new(1)
     gamepad = Joystick:new(2)
 
+    robotDrive = DifferentialDrive:new(leftMotor, rightMotor)
 end
 
 function robot.teleopPeriodic()
-    leftMotor.set(leftStick:getAxis(JoystickAxes.Y) + rightStick:getAxis(JoystickAxes.X))
-    rightMotor.set(leftStick:getAxis(JoystickAxes.Y) - rightStick:getAxis(JoystickAxes.X))
+    robotDrive:arcadeDrive(-leftStick:getAxis(JoystickAxes.Y), rightStick:getAxis(JoystickAxes.X))
 end
 
 function robot.autonomousInit()
@@ -22,7 +22,7 @@ function robot.autonomousPeriodic()
 end
 
 function deadband(joystickVal)
-    if joystickVal < 0.05 and joystickVal > -0.05
+    if joystickVal < 0.05 and joystickVal > -0.05 then
         return 0
     else
         return joystickVal
